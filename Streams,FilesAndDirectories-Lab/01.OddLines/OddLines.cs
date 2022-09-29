cb040b4a529c9688a6ee90599e3c60f530772fef
Stefan Rusev <StefanGRusev91@gmail.com>
2022-09-29 19:44:01 +0300

    StreamsFilesAndDirectories

---
+++ Streams,FilesAndDirectories-Lab/01.OddLines/OddLines.cs
@@ -0,0 +1,39 @@
+﻿namespace OddLines
+{
+    using System.IO;
+	
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int lineNumber = 0;
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (lineNumber % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        lineNumber++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StreamsFilesAndDirectories-Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int lettersCount = lines[i].Count(ch => char.IsLetter(ch));
+                int symbolCount = lines[i].Count(ch => char.IsPunctuation(ch));
+
+                stringBuilder.AppendLine($"Line {i + 1}: {lines[i]} ({lettersCount})({symbolCount})");
+            }
+            File.WriteAllText(outputFilePath, stringBuilder.ToString());
+        }
+    }
+}
