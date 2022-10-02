d92ed42973a2ef82746a0fb21415ad739308a1fd
Stefan Rusev <StefanGRusev91@gmail.com>
2022-10-02 18:40:51 +0300

    FunctionalProgramming-Lab

---
+++ FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parsingTheNumber = n => int.Parse(n);
+
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parsingTheNumber)
+                .ToArray();
+
+            Console.WriteLine(input.Length);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
---
+++ FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isUpperCapital = w => char.IsUpper(w[0]);
+            Console.WriteLine(string.Join(Environment.NewLine, Array
+                .FindAll(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries), isUpperCapital)));
+        }
+    }
+}
